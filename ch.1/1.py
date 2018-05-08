4f5b3a22787154d6cb661cf4bc1869ed71041687
Michael <mikeqassis@gmail.com>
2018-05-07 22:56:36 -0700

    Update 1.py

--- ch.1/1.py
+++ ch.1/1.py
@@ -1,3 +1,4 @@
+'''Write a function is_multiple(n,m), that takes two integers and returns True if n is a multiple of m and False otherwise.'''
 def is_multiple(n,m):
     if n % m == 0:
         return True
